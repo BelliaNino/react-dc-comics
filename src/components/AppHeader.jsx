2597de396527665997eb3e363d30f48d7ad26a1b
BelliaNino <bellianino1996@gmail.com>
2026-03-06 20:56:37 +0100

    add layout header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,7 +4,21 @@ import Logo from "../assets/dc-logo.png"
 export default function AppHeader() {
     return (
         <header>
-            Header
+            <div className="container">
+                <img src={Logo} alt="" />
+                <nav>
+                    <a href="">CHARACTERS</a>
+                    <a href="">COMICS</a>
+                    <a href="">MOVIES</a>
+                    <a href="">TV</a>
+                    <a href="">GAMES</a>
+                    <a href="">COLLECTIBLES</a>
+                    <a href="">VIDEOS</a>
+                    <a href="">FANS</a>
+                    <a href="">NEWS</a>
+                    <a href="">SHOP</a>
+                </nav>
+            </div>
         </header>
     )
 }
